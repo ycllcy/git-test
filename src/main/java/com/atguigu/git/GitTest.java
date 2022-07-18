8a5b29a2aaa614ac803e4e443cd4e049d99ca3cf
LcccY <lcy0101001@gmail.com>
2022-07-18 22:44:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LcccY
+ * @create 2022-07-18-22:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
